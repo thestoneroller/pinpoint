8ce18c3b6c1d06ac68b43b138838a6de01ef48a7
thestoneroller <marnitanmayadithya@gmail.com>
2025-08-29 16:31:20 +0530

    chore: add color palatte and favicons

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
+  <h1 class="text-foreground text-3xl">You did it!</h1>
+  <p class="text-muted-foreground">The wire</p>
 
-<style scoped></style>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="text-foreground h-6 w-6">
+    <path fill="currentColor"
+      d="M5 2h4v2H7v2H5V2Zm0 10H3V6h2v6Zm2 2H5v-2h2v2Zm2 2v-2H7v2H3v-2H1v2h2v2h4v4h2v-4h2v-2H9Zm0 0v2H7v-2h2Zm6-12v2H9V4h6Zm4 2h-2V4h-2V2h4v4Zm0 6V6h2v6h-2Zm-2 2v-2h2v2h-2Zm-2 2v-2h2v2h-2Zm0 2h-2v-2h2v2Zm0 0h2v4h-2v-4Z" />
+  </svg>
+</template>
