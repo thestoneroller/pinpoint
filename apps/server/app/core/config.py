4db577d4de25fc131da84b4aa06fd18c66e0101d
thestoneroller <marnitanmayadithya@gmail.com>
2025-09-03 08:18:06 +0530

    chore: change .env path

--- apps/server/app/core/config.py
+++ apps/server/app/core/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     """Settings for the application."""
 
     model_config = SettingsConfigDict(
-        env_file="../.env",
+        env_file="../../.env",
         env_ignore_empty=True,
         extra="ignore",
     )
