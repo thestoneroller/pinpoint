bfe8c9e2b8578c19e678894ab0f03de24180b1c8
thestoneroller <marnitanmayadithya@gmail.com>
2025-09-06 18:16:45 +0530

    feat: add concurrency to fetch comments

--- apps/server/app/services/github.py
+++ apps/server/app/services/github.py
@@ -1,3 +1,4 @@
+import asyncio
 from githubkit import GitHub
 from githubkit.versions.latest.models import IssueSearchResultItem, IssueComment
 from ..core.config import settings
@@ -14,29 +15,36 @@ async def search_issues(
     Search for issues in a repository and return them.
     """
 
+    issue_tasks = []
+
+    async with asyncio.TaskGroup() as tg:
+        for query in queries:
+            issue_task = tg.create_task(
+                gh.rest.search.async_issues_and_pull_requests(
+                    q=f"repo:{repo} is:issue {query}",
+                    order="desc",
+                    sort="reactions",
+                )
+            )
+            issue_tasks.append(issue_task)
+
     issues = []
-
-    for query in queries:
-        resp = await gh.rest.search.async_issues_and_pull_requests(
-            q=f"repo:{repo} is:issue {query}",
-            order="desc",
-            sort="reactions",
-        )
-        print(f"repo:{repo} is:issue {query}")
-        issues.extend(resp.parsed_data.items)
-
-    # Remove duplicate issues
     unique_issues = {}
+
+    for task in issue_tasks:
+        response = task.result()
+        issues.extend(response.parsed_data.items)
+
     for issue in issues:
         if issue.id not in unique_issues:
             unique_issues[issue.id] = issue
 
-    issues = list(unique_issues.values())
+    unique_issues = list(unique_issues.values())
 
-    issue_numbers = [issue.number for issue in issues]
-    print([f"{issue.number}- {issue.title}" for issue in issues])
+    issue_numbers = [issue.number for issue in unique_issues]
+    print([f"{issue.number}- {issue.title}" for issue in unique_issues])
 
-    return issues, issue_numbers
+    return unique_issues, issue_numbers
 
 
 async def get_issues_comments(
@@ -45,13 +53,22 @@ async def get_issues_comments(
 
     username, repo = repo.split("/")
 
-    comments = []
+    comment_tasks = []
 
-    for issue_num in issue_numbers:
-        resp = await gh.rest.issues.async_list_comments(
-            owner=username, repo=repo, issue_number=issue_num
-        )
-        comments.extend(resp.parsed_data)
+    async with asyncio.TaskGroup() as tg:
+        for issue_num in issue_numbers:
+            fetch_comments_task = tg.create_task(
+                gh.rest.issues.async_list_comments(
+                    owner=username, repo=repo, issue_number=issue_num
+                )
+            )
+            comment_tasks.append(fetch_comments_task)
+
+    # Flatten the comments from all responses
+    comments = []
+    for task in comment_tasks:
+        response = task.result()
+        comments.extend(response.parsed_data)
 
     return comments
 
