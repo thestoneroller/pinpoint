a5b8edeb3ba46d4b49668346d0547e0fb099f15e
thestoneroller <marnitanmayadithya@gmail.com>
2025-09-13 10:16:13 +0530

    feat: add grain and gradient

--- apps/web/src/views/Home.vue
+++ apps/web/src/views/Home.vue
@@ -12,22 +12,48 @@ const navigateToSearch = () => {
 
 <template>
   <Navbar />
+
+  <!-- Background Gradient -->
+  <div class="absolute inset-0 -z-20 h-dvh w-full overflow-hidden opacity-90">
+    <div class="absolute inset-0 mt-0 h-full">
+      <div
+        class="absolute left-1/2 aspect-square h-[105%] w-[125%] -translate-x-1/2 overflow-hidden sm:-bottom-40 sm:h-full md:w-[100%] lg:w-[100%] xl:w-[100%] 2xl:mx-auto"
+        style="
+          background-image: url('/background/gradient.png');
+          background-size: cover;
+          background-repeat: no-repeat;
+          background-position: center top;
+        "
+      ></div>
+    </div>
+  </div>
+
+  <div
+    class="absolute inset-0 -z-10 opacity-5"
+    style="
+      background-image: url(/background/grain.png);
+      background-size: 100px 100px;
+      background-repeat: repeat;
+      background-position: left top;
+    "
+  ></div>
+
   <!--  Hero Heading & Subheading -->
-  <div class="mt-30 flex flex-col items-center justify-center gap-4 md:mt-20">
+  <div class="mt-30 flex flex-col items-center justify-center gap-4 md:mt-28">
     <h1
-      class="font-alpha text-foreground xs:text-3xl w-96 text-center text-2xl text-pretty sm:w-[28rem] sm:text-[2rem] md:w-[40rem] md:text-[2.5rem]"
+      class="xs:text-3xl text-foreground mx-auto line-clamp-1 flex w-fit gap-3 text-center text-2xl tracking-tighter text-pretty sm:w-[28rem] sm:text-[2rem] md:min-w-fit md:text-[3rem]"
     >
-      Find solutions to bugs in external libraries from GitHub issues.
+      Find solutions from GitHub.
     </h1>
     <p
-      class="text-muted-foreground hidden w-64 text-center text-xs text-pretty sm:block sm:w-[30rem] sm:text-base md:w-[43rem]"
+      class="text-foreground/80 line-clamp-1 hidden min-w-fit text-center text-xs sm:block sm:text-base"
     >
-      The bug you’ve been stuck on all day isn’t unique. Discover the fix in GitHub issues.
+      Search across Github issues and discover how others fixed the same problems.
     </p>
   </div>
   <!-- Prompt -->
   <div
-    class="border-line-primary/50 bg-background shadow-fill/80 relative mt-10 flex w-[90vw] flex-col gap-2 rounded-sm border p-4 shadow-sm md:mt-12 md:w-[42rem] lg:w-[48rem]"
+    class="border-line-secondary bg-background relative mt-10 flex w-[90vw] flex-col gap-2 rounded-2xl border p-4 shadow-xl md:mt-12 md:w-[42rem] lg:w-[48rem]"
   >
     <textarea
       id="hs-textarea-ex-1"
@@ -43,11 +69,10 @@ const navigateToSearch = () => {
         <div class="flex items-center">
           <button
             type="button"
-            class="active:bg-fill/70 hover:bg-fill/70 border-line-primary/60 flex shrink-0 cursor-pointer items-center justify-baseline gap-px rounded-sm border px-3 py-2 transition-transform focus:z-10 focus:outline-hidden active:translate-y-px active:scale-98"
+            class="active:bg-fill border-line-secondary flex shrink-0 cursor-pointer items-center justify-baseline gap-2 rounded-lg border px-4 py-2 transition-all duration-100 hover:shadow-sm focus:z-10 focus:outline-hidden active:translate-y-px active:scale-98"
           >
             <Icon name="github" class="fill-foreground h-4 w-4" />
-            <Icon name="slash" class="fill-foreground h-4 w-4" />
-            <p class="text-foreground text-xs sm:text-sm">Add Repo</p>
+            <p class="text-foreground text-xs sm:text-sm">Select Repo</p>
           </button>
         </div>
 
@@ -56,7 +81,7 @@ const navigateToSearch = () => {
           <button
             type="button"
             @click="navigateToSearch"
-            class="bg-brand hover:bg-brand/90 focus:bg-brand/90 inline-flex size-8 shrink-0 cursor-pointer items-center justify-center rounded-sm text-white transition-transform focus:z-10 focus:outline-hidden active:translate-y-px active:scale-98"
+            class="bg-brand hover:bg-brand/90 focus:bg-brand/90 inline-flex size-8 shrink-0 cursor-pointer items-center justify-center rounded-lg text-white transition-transform focus:z-10 focus:outline-hidden active:translate-y-px active:scale-98"
           >
             <Icon name="arrow-right" />
           </button>
